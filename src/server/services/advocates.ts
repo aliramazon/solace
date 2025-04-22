f8d99a0e18e010a8d0e066885707afb7f292a765
Ali Ramazon <mr.safarali@gmail.com>
2025-04-21 20:07:45 -0600

    Support cursor based pagination

--- src/server/services/advocates.ts
+++ src/server/services/advocates.ts
@@ -1,11 +1,31 @@
-import db from "../../db";
+import { asc, gt, type InferSelectModel } from "drizzle-orm";
+import { db } from "../../db";
 import { advocates } from "../../db/schema";
 
-class Advocates {
-  async getAll() {
-    const data = await db.select().from(advocates);
-    return data;
+type GetAllParams = {
+  limit: string | null;
+  cursor: string | null;
+};
+
+class AdvocatesService {
+  async getAll({ limit, cursor }: GetAllParams): Promise<{
+    data: InferSelectModel<typeof advocates>[];
+    nextCursor: number | null;
+  }> {
+    const parsedCursor = cursor ? parseInt(cursor, 10) : undefined;
+    const parsedLimit = limit ? parseInt(limit, 10) : 20;
+
+    const data = await db
+      .select()
+      .from(advocates)
+      .where(parsedCursor ? gt(advocates.id, parsedCursor) : undefined)
+      .orderBy(asc(advocates.id))
+      .limit(parsedLimit);
+
+    const nextCursor = data.length > 0 ? data[data.length - 1].id : null;
+
+    return { data, nextCursor };
   }
 }
 
-export const advocatesService = new Advocates();
+export const advocatesService = new AdvocatesService();
