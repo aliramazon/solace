86e4f43ee320d02b53432bdba0a031086ae1708f
Ali Ramazon <mr.safarali@gmail.com>
2025-04-26 19:32:58 -0600

    Test main branch deployment

--- src/app/pages/advocates/advocates-header.tsx
+++ src/app/pages/advocates/advocates-header.tsx
@@ -7,7 +7,7 @@ export const AdvocatesHeader = () => {
         </h1>
         <p className="advocates__hero-subtext">
           Find an advocate who will help untangle your healthcare by phone or
-          video—no matter what you need—<span>covered by Medicare</span>.
+          video—no matter what you need—<span>covered by Medicare!</span>
         </p>
       </div>
     </div>
