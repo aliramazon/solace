572f1c28db081a23aadcc4a88d2a5f29c2e9de92
Ali Ramazon <mr.safarali@gmail.com>
2025-05-04 22:59:02 -0600

    Switch to offset based pagination

--- src/server/types/pagination.ts
+++ src/server/types/pagination.ts
@@ -2,6 +2,5 @@ export type PaginationDirection = "prev" | "next" | null;
 
 export interface Pagination {
   limit: string | null;
-  cursor: string | null;
-  direction: PaginationDirection;
+  offset: string | null;
 }
